3a2aa3958795ff317369b29cb03eeeeabe18b544
mp3itk <50299485+mp3itk@users.noreply.github.com>
2020-06-24 13:16:16 +0300

    Update backupDockerContainers.sh

--- backupDockerContainers.sh
+++ backupDockerContainers.sh
@@ -14,24 +14,26 @@ dbf='/dbf'
 
 
 #Remote information
-remoteSahre="Remote share: example - //xx.xx.xx.xx/shareFolder"
-shareUsername="username"
-shareDomain="domain"
-sharePassword="password"
-localFolder="temp folder which contain exported files"
+remoteSahre="//15.17.199.47/dockerBackup"
+shareUsername="danielm"
+shareDomain="inr"
+sharePassword="052707Dd"
+localFolder="/tmp/mountBackupScript"
 
 logfile="logs.log"
+tempImages="/var/log/backupTempScript.txt"
 
 saveAsImage()
 {
 
     #Export Containers to Images
-    clear
+
     docker ps -a --format '{{.ID}} {{.Names}}' | while read line ;\
         do c1=`echo $line | awk '{print $1}'`;\
            c2=`echo $line | awk '{print $2}'`;\
                echo "exporting: $c2 as Image" | tee  -a $dbf/$logfile;\
     docker commit $c1 $c2.$currentDate:backup; done
+    print "\n\n"
     return
 }
 
@@ -40,23 +42,28 @@ saveAsImage()
 export()
 {
     #Export images as Tar file
-   clear
 
    docker images |\
         cut -d" " -f 1 |\
         tail -n +2 | \
         while read line ; \
-        do echo "exporting: $line as tar file" | tee -a $dbf/$logfile ; docker save -o $dbf/$line.tar $line ;\
+        do  echo "$line" >> $tempImages ;\
+        echo "exporting: $line as tar file" | tee -a $dbf/$logfile ; docker save -o $dbf/$line.tar $line ;\
     done
+    printf "\n\n"
     return
 }
 
 
 removeAllImages()
 {
-    clear
-    docker images | awk '{print $3}' | while read line ; do echo "removing old image $line" | tee -a $dbf/$logfile;  docker rmi $line ; done
-    return
+
+        if [ -s $tempImages ]; then
+docker images  --format "{{.Repository}} {{.ID}}" | while read line ; do v1=`echo $line | awk {'print $1'}` v2=`echo $line | awk '{print $2}'`; for i in `cat $tempImages` ;do if [ "$v1" == "$i" ] ; then docker rmi $v2  ;fi  ; done ;done
+        fi
+        echo > $tempImages
+        printf "\n\n"
+        return
 }
 
 
@@ -64,9 +71,10 @@ removeAllImages()
 uploadTarFilesAsImages()
 
 {
-    clear
+
 
     cd $dbf ; ls | while read line ; do echo "Uploading: $line as image" | tee  -a $dbf/$logfile ; docker load -i $dbf/$line ; done
+    printf "\n\n"
     return
 }
 
@@ -74,29 +82,35 @@ uploadTarFilesAsImages()
 
 mountShare()
 {
+    if [ ! -d $localFolder ];then mkdir $localFolder ;fi
     #Check if the share folder allready mounted
     umount $localFolder ||  mount -t cifs $remoteSahre -o username=$shareUsername,domain=$shareDomain,password=$sharePassword $localFolder
     clear
     echo "copy files to $remoteSahre\n======================\n" | tee  -a $dbf/$logfile
     rsync -v $dbf/*  $localFolder | tee  -a $dbf/$logfile
     umount $localFolder
+    printf "\n\n"
     return
 }
 
 start()
 {
-    echo "==== remove all old images ===="  | tee  -a $dbf/$logfile ; echo "===================================\n\n"
+    echo "==== remove all old images ===="  | tee  -a $dbf/$logfile ; echo "==================================="
     removeAllImages
 
-    echo "==== Start export containers as images ====" | tee  -a $dbf/$logfile ; echo "===================================\n\n"
+    echo "==== Start export containers as images ====" | tee  -a $dbf/$logfile ; echo "==================================="
     saveAsImage
 
-    echo "==== Start Export images as Tar files ====" | tee  -a $dbf/$logfile ; echo "===================================\n\n"
+    echo "==== Start Export images as Tar files ====" | tee  -a $dbf/$logfile ; echo "==================================="
     export
 
-    echo "==== Start upload all tar as images ====" | tee  -a $dbf/$logfile ; echo "===================================\n\n"
+    echo "==== Start upload all tar as images ====" | tee  -a $dbf/$logfile ; echo "==================================="
     uploadTarFilesAsImages
+
+    echo "==== Start upload all tar's files into share folder ====" | tee  -a $dbf/$logfile ; echo "==================================="
+    mountShare
 }
+
 
 
 
